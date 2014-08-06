399a8d58fe5e26593b056271399c1c53a9884f1a
randallreedjr <randallreedjr@gmail.com>
2014-08-06 15:35:25 -0400

    Fix up login flow

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   resources :users
 
-  root 'campaigns#index'
+  root 'pages#home'
   get '/login' => 'sessions#new'
   get '/auth/twitter/callback' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
