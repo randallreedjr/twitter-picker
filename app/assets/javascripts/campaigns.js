b95e06fa61520901527be8615bd4a83116555055
randallreedjr <randallreedjr@gmail.com>
2014-08-12 18:30:44 -0400

    Use javascript to dynamically add/remove prizes

--- app/assets/javascripts/campaigns.js
+++ app/assets/javascripts/campaigns.js
@@ -6,15 +6,18 @@ $('form').submit(function () {
 $( document ).ready(function(){
   $('.hide-form').hide();
 });
+
 $( document ).ready(function(){
-$('.add-prize').click(function(e){
-  e.preventDefault();
-  $('#prizes > ul').append('<li><p><label for="campaign_prizes_attributes_name">Name</label><input id="campaign_prizes_attributes_name" name="campaign[prizes_attributes][][name]" type="text" /></p><p><label for="campaign_prizes_attributes_how_many">How many</label><input id="campaign_prizes_attributes_number" name="campaign[prizes_attributes][][number]" type="number" /></p><button class="delete-prize">Delete</button></li>');
+  $('.add-prize').click(function(e){
+    e.preventDefault();
+    var num = parseInt($('#prizes > ul > li > p > input').last().attr('name').slice(28,30).replace(']','')) + 1;
+    $('#prizes > ul').append('<li><p><label for="campaign_prizes_attributes_'+num+'_name">Name</label><input id="campaign_prizes_attributes_'+num+'_name" name="campaign[prizes_attributes]['+num+'][name]" type="text" /></p><p><label for="campaign_prizes_attributes_'+num+'_how_many">How many</label><input id="campaign_prizes_attributes_'+num+'_number" name="campaign[prizes_attributes]['+num+'][number]" type="number" /></p><button class="delete-prize">Delete</button></li>');
+  });
 });
 
-$('.delete-prize').click(function(e){
-  e.preventDefault();
-  console.log('delete prize');
-  alert('delete prize');
-});
+$( document ).ready(function(){
+  $('form').on('click','.delete-prize',function(e){
+    e.preventDefault();
+    $(this).parent().remove()
+  });
 });
