9cc014b42600decf4bb79ddcc6661cacc2417ff7
simonseroussi <simon.seroussi@gmail.com>
2014-08-12 15:54:49 -0400

    added number field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140812004522) do
+ActiveRecord::Schema.define(version: 20140812185930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20140812004522) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "number"
   end
 
   create_table "tweets", force: true do |t|
