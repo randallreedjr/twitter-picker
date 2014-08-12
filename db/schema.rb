86162d05e3e90d1354ebdced78e1f91a6bcc137e
randallreedjr <randallreedjr@gmail.com>
2014-08-12 15:58:07 -0400

    Remove prize description

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140812190339) do
+ActiveRecord::Schema.define(version: 20140812195403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(version: 20140812190339) do
   create_table "prizes", force: true do |t|
     t.integer  "campaign_id"
     t.string   "name"
-    t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
