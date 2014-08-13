9264b34bba8ae78e936bdb0ca42495cff5e23307
randallreedjr <randallreedjr@gmail.com>
2014-08-13 13:36:12 -0400

    Fix javascript; update styling and show partial for prizes.

--- app/assets/javascripts/campaigns.js
+++ app/assets/javascripts/campaigns.js
@@ -11,7 +11,7 @@ $( document ).ready(function(){
   $('.add-prize').click(function(e){
     e.preventDefault();
     var num = parseInt($('#prizes > ol > li > p > input').last().attr('name').slice(28,30).replace(']','')) + 1;
-    $('#prizes > ol').append('<li class="prize"><p><label for="campaign_prizes_attributes_'+num+'_name" class="prize-label">Name</label><input id="campaign_prizes_attributes_'+num+'_name" name="campaign[prizes_attributes]['+num+'][name]" type="text" /></p><p><label for="campaign_prizes_attributes_'+num+'_how_many" class="prize-label">How many</label><input id="campaign_prizes_attributes_'+num+'_number" name="campaign[prizes_attributes]['+num+'][number]" type="number" /></p><button class="btn btn-danger delete-prize">Delete</button></li>');
+    $('#prizes > ol').append('<li class="prize"><p><label for="campaign_prizes_attributes_'+num+'_name" class="prize-label">Name</label><input id="campaign_prizes_attributes_'+num+'_name" name="campaign[prizes_attributes]['+num+'][name]" type="text" /></p><p><label for="campaign_prizes_attributes_'+num+'_how_many" class="prize-label">How many</label><input id="campaign_prizes_attributes_'+num+'_number" name="campaign[prizes_attributes]['+num+'][number]" type="number" value="1" readonly /></p><button class="btn btn-danger delete-prize">Delete</button></li>');
   });
 });
 
