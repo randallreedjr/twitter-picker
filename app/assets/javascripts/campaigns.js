fe1270209e9d079f55abba86f94fb1d39ac0f682
randallreedjr <randallreedjr@gmail.com>
2014-08-12 21:52:48 -0400

    Correctly style delete button when new prize is added via JS

--- app/assets/javascripts/campaigns.js
+++ app/assets/javascripts/campaigns.js
@@ -11,7 +11,7 @@ $( document ).ready(function(){
   $('.add-prize').click(function(e){
     e.preventDefault();
     var num = parseInt($('#prizes > ul > li > p > input').last().attr('name').slice(28,30).replace(']','')) + 1;
-    $('#prizes > ul').append('<li><p><label for="campaign_prizes_attributes_'+num+'_name">Name</label><input id="campaign_prizes_attributes_'+num+'_name" name="campaign[prizes_attributes]['+num+'][name]" type="text" /></p><p><label for="campaign_prizes_attributes_'+num+'_how_many">How many</label><input id="campaign_prizes_attributes_'+num+'_number" name="campaign[prizes_attributes]['+num+'][number]" type="number" /></p><button class="delete-prize">Delete</button></li>');
+    $('#prizes > ul').append('<li><p><label for="campaign_prizes_attributes_'+num+'_name">Name</label><input id="campaign_prizes_attributes_'+num+'_name" name="campaign[prizes_attributes]['+num+'][name]" type="text" /></p><p><label for="campaign_prizes_attributes_'+num+'_how_many">How many</label><input id="campaign_prizes_attributes_'+num+'_number" name="campaign[prizes_attributes]['+num+'][number]" type="number" /></p><button class="btn btn-danger delete-prize">Delete</button></li>');
   });
 });
 
